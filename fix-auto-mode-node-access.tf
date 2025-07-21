d02f193f9f687ad60abccf28306755ef2a7a6727
Diego Barrios <diego.barrios@angecom.cl>
2025-07-21 11:12:43 -0300

    initial files

--- fix-auto-mode-node-access.tf
+++ fix-auto-mode-node-access.tf
@@ -1,17 +1,17 @@
-resource "kubernetes_cluster_role_binding" "eks_admin_rbac" {
-  metadata {
-    name = "eks-admin"
-  }
+# resource "kubernetes_cluster_role_binding" "eks_admin_rbac" {
+#   metadata {
+#     name = "eks-admin"
+#   }
 
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
 
-  subject {
-    kind      = "User"
-    name      = "arn:aws:iam::224607388582:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_GC-AdministratorAccess_c7345ddd4f8d6c19"
-    api_group = "rbac.authorization.k8s.io"
-  }
-}
+#   subject {
+#     kind      = "User"
+#     name      = "arn:aws:iam::224607388582:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_GC-AdministratorAccess_c7345ddd4f8d6c19"
+#     api_group = "rbac.authorization.k8s.io"
+#   }
+# }
